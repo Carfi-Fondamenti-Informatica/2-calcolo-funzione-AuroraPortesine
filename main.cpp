e7567c993840e2990195be036345bc1c99624177
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-03-29 17:31:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, x=0, y=0;
+    cin >> a >> b >> x >> y;
+
+    if(x<0 and y>0){
+        cout << " z=a*x-b*y " << endl;
+    }else if(x>=0 and y<=0){
+        cout << " z=a*(x^2)–b*y " << endl;
+    }else{
+        cout << " z=a*x+b*(y^2) " << endl;
+    }
 }
