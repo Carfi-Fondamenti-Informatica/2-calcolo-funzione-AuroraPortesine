2f4c1e87745080ff0056c3cf146f7dd582c58034
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-03-29 17:43:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main() {
     float a=0, b=0, x=0, y=0;
+    int z=0;
     cin >> a >> b >> x >> y;
 
     if(x<0 and y>0){
-        cout << " z=a*x-b*y " << endl;
+        z=a*x-b*y;
     }else if(x>=0 and y<=0){
-        cout << " z=a*(x^2)–b*y " << endl;
+        z=a*(x*x) - b*y;
     }else{
-        cout << " z=a*x+b*(y^2) " << endl;
+        z=a*x+b*(y*y);
     }
+    cout << " \nz=" << z<< endl;
 }
